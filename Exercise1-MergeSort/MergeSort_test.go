6c1d7a5b7fd577afde282952c20c996a9964e7b2
Tsahi Edri <TsahiEdri1@gmail.com>
2022-05-13 15:07:49 +0300

    added tests for mergesort

--- Exercise1-MergeSort/MergeSort_test.go
+++ Exercise1-MergeSort/MergeSort_test.go
@@ -26,12 +26,34 @@ func TestMerge(t *testing.T) {
 		{name: "left empty", left: []int{}, right: []int{1, 5, 7}, merged: []int{1, 5, 7}},
 		{name: "right empty", left: []int{1, 5, 7}, right: []int{}, merged: []int{1, 5, 7}},
 		{name: "duplicate numbers", left: []int{1, 1, 4, 6}, right: []int{2, 4, 5, 6}, merged: []int{1, 1, 2, 4, 4, 5, 6, 6}},
+		{name: "negative numbers included", left: []int{-7, 1, 9}, right: []int{-2, 5, 8}, merged: []int{-7, -2, 1, 5, 8, 9}},
 	}
 	for _, tt := range mergeArraysTests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := Merge(tt.left, tt.right)
 			if !Equal(got, tt.merged) {
-				t.Errorf("failed int test %q", tt.name)
+				t.Errorf("failed in test %q", tt.name)
+			}
+		})
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	mergeSortArraysTests := []struct {
+		name     string
+		source   []int
+		expected []int
+	}{
+		{name: "basic test", source: []int{8, 4, 6, 2, 1, 6, 9}, expected: []int{1, 2, 4, 6, 6, 8, 9}},
+		{name: "empty", source: []int{}, expected: []int{}},
+		{name: "negative numbers", source: []int{-8, -4, -6, -2, -1, -6, -9}, expected: []int{-9, -8, -6, -6, -4, -2, -1}},
+		{name: "both positive and negative", source: []int{-8, 4, -6, 2, -1, 6, 9}, expected: []int{-8, -6, -1, 2, 4, 6, 9}},
+	}
+	for _, tt := range mergeSortArraysTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := MergeSort(tt.source)
+			if !Equal(got, tt.expected) {
+				t.Errorf("failed in test %q", tt.name)
 			}
 		})
 	}
